207840b5c52f415d05b740eba84ff585b7313330
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-09 12:16:16 +0300

    add jwt cookie provider

---
+++ src/Provider/JWT/JwtCookieProviderInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Evrinoma\SecurityBundl\Provider\JWT;
+
+use Symfony\Component\HttpFoundation\Cookie;
+
+interface JwtCookieProviderInterface
+{
+    public function createCookie(string $jwt, ?string $name = null, $expiresAt = null, ?string $sameSite = null, ?string $path = null, ?string $domain = null, ?bool $secure = null, ?bool $httpOnly = null, array $split = []): Cookie;
+}
